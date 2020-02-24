44e921d343bbd6939be2d428c7d73e275f793c25
Karim Azib <karim.azib17@gmail.com>
2020-02-24 16:43:26 +0100

    new view

--- app/controllers/dashboards_controller.rb
+++
@@ -1,3 +0,0 @@
-class DashboardsController < ApplicationController
-
-end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+
+  end
+
+  def dashboard
+
+
   end
 end
