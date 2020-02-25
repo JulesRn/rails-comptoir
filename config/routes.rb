f896ed93118415fbd351c41894616b94550fd09e
juliedakouré <julie.dakoure@outlook.fr>
2020-02-25 11:32:02 +0100

    index done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :meetings, only: :show
   resources :likes, only: :create
+  resources :users, only: [:create, :index, :show, :edit, :update, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
