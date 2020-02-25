eefb562ad9350590efb7e7bff7d7c84ce1165bae
cd ~/code/$GITHUB_USERNAME/dotfiles <jfournial@gmail.com>
2020-02-25 16:48:07 +0100

    pleins de trucs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :meetings, only: :show
+  resources :meetings, only: [:show, :destroy]
   resources :likes, only: :create
   resources :users, only: [:create, :index, :show, :edit, :update, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
