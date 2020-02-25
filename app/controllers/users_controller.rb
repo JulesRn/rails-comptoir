a29f5ae7a3253798dba93c85330cd7a6080d0ea6
Jules Ronne <jules.ronne@gmail.com>
2020-02-25 12:25:25 +0100

    ajout method create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to user_path(@user)
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
