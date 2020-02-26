6aa3e0355537400d33da284bb4f138be450a78d9
juliedakouré <julie.dakoure@outlook.fr>
2020-02-26 17:50:40 +0100

    carousel

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :user2_meetings, :class_name => 'Meeting', :foreign_key => 'user2_id'
   has_many :availabilities
   has_many :liked_users, :class_name => 'Like', :foreign_key => 'liked_user_id'
-  validates :email, :name, :description, :age, :height, :sex, :sexual_orientation, :photo, presence: true
+  validates :email, :description, :name, :age, :height, :sex, :sexual_orientation, presence: true
 
   def afterworks_dispos
     avails = self.availabilities.pluck(:days, :afterwork).delete_if { |arr| arr.last == false }
