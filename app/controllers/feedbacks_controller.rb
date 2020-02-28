15ce017512c55f9b7dd63d6fc4d26dd9630e400e
Karim Azib <karim.azib17@gmail.com>
2020-02-28 12:13:46 +0100

    view + env

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,16 +2,26 @@ class FeedbacksController < ApplicationController
 
    def show
     @user = current_user
-  end
+   end
 
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to profile_path
-    else
-      render 'show'
+  # def update
+  #   @user = current_user
+  #   if @user.update(user_params)
+  #     redirect_to profile_path
+  #   else
+  #     render 'show'
+  #   end
+  # end
+
+    def new
+      @feedback = Feedback.new
     end
-  end
 
+    def create
+      @meeting = Meeting.find(params[:meeting_id])
+      @meeting.status = "occured"
+      @user = current_user
+      @feedback = Feedback.new(user: @user, meeting: @meeting)
+    end
 
 end
