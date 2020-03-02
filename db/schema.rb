1f70554b230f5e2515cde2bcea1f66d65005888b
juliedakouré <julie.dakoure@outlook.fr>
2020-03-02 11:20:54 +0100

    add geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_181023) do
+ActiveRecord::Schema.define(version: 2020_03_02_084029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 2020_03_01_181023) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "unlikes", force: :cascade do |t|
